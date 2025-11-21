45c8266c2467061816ab376b4e1729351da1966a
Fatima <fatima.ali202008@gmail.com>
2025-11-20 23:51:08 -0500

    description for synthetic curves

---
+++ .ipynb_checkpoints/model-checkpoint.py
@@ -0,0 +1,57 @@
+import timeit
+from astropy import constants as c
+from astropy import units as u
+from kilonova_heating_rate import lightcurve
+from matplotlib import pyplot as plt
+import numpy as np
+import synphot
+
+mass = 0.05 * u.Msun
+velocities = np.asarray([0.1, 0.2, 0.3, 0.4]) * c.c
+opacities = np.asarray([10.0, 3.0, 0.5]) * u.cm**2 / u.g
+n = 4.5
+t = np.geomspace(0.02, 10) * u.day
+
+L, T, r = lightcurve(t, mass, velocities, opacities, n)
+
+# Benchmark it
+timing = int(np.round(1e6 * np.median(timeit.repeat(
+    'lightcurve(t, mass, velocities, opacities, n)',
+    globals=globals(), number=1, repeat=10))))
+
+# Evaluate flux in band at 40 Mpc for a few different filters.
+DL = 40 * u.Mpc
+bandpass_labels = 'UBVRI'
+bandpasses = [
+    synphot.SpectralElement.from_filter(f'johnson_{label}')
+    for label in bandpass_labels]
+seds = [
+    synphot.SourceSpectrum(synphot.BlackBody1D, temperature=TT)
+    * np.pi * (rr / DL).to(u.dimensionless_unscaled)**2
+    for TT, rr in zip(T, r)]
+
+# Convert the same observations to AB magnitudes for plotting/comparison.
+abmags = [
+    [synphot.Observation(sed, bandpass).effstim(u.ABmag).value for sed in seds]
+    for bandpass in bandpasses]
+
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True, figsize=(4, 6))
+fig.suptitle(f'Run time: {timing} µs')
+ax1.plot(t, T)
+ax2.plot(t, L)
+for label, abmag in zip(bandpass_labels, abmags):
+    ax3.plot(t, abmag, label=label.upper())
+ax3.set_xlabel(f'Time ({t.unit})')
+ax1.set_ylabel(f'Temperature ({T.unit})')
+ax2.set_ylabel(f'Luminosity ({L.unit})')
+ax3.set_ylabel('Apparent mag (AB)')
+ax3.set_ylim(17,25)
+ax1.set_xlim(0.01, 10)
+ax1.set_ylim(1e3, 1e4)
+ax2.set_ylim(1e40, 1e42)
+ax3.invert_yaxis()
+ax1.set_yscale('log')
+ax2.set_yscale('log')
+ax3.legend()
+fig.tight_layout()
+plt.show()
